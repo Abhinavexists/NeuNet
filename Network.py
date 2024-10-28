c5d1a08fbceff9d05f1ef266d53bf9b84124a3f4
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-10-28 13:02:00 +0530

    dot product

--- Network.py
+++ Network.py
@@ -3,15 +3,23 @@ import numpy as np
 import matplotlib
 
 inputs = [1.2 , 3.4 , 5.9 , 3.6] #output numbers treated as input for upcoming layers
-weights1 = [3.9 , 7.6 , 4.3 , 1.0] # weights of each input (random number)
-weights2 = [3.6 , 7.0 , 5.0 , 1.7]
-weights3 = [6.9 , 6.6 , 4.9 , 1.8]
-bias1 = 3 #unique bias
-bias2 = 2
-bias3 = 1
+weights = [[3.9 , 7.6 , 4.3 , 1.0], # weights of each input (random number)
+          [3.6 , 7.0 , 5.0 , 1.7],
+          [6.9 , 6.6 , 4.9 , 1.8]]
 
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1, # list of output layer 
-inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
+biases = [3,2,1] #unique bias
+# weights = [3.9 , 7.6 , 4.3 , 1.0]
+# bias = 2
+
+output = np.dot(weights , inputs) + biases
 print(output)
 
+# layer_outputs = []
+# for neuron_weights , neuron_bias in zip(weights , biases): # zip combines 2 list into a list of list
+#   neuron_output = 0
+#   for n_input , weight in zip(inputs , neuron_weights):
+#     neuron_output += n_input*weight
+#   neuron_output += neuron_bias
+#   layer_outputs.append(neuron_output)
+
+# print(layer_outputs)
