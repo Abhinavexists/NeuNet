8eafd651c1496b210696c2521b75c57a19b39599
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-10-28 19:28:54 +0530

    Dense Layer implementation

--- Network.py
+++ Network.py
@@ -2,17 +2,47 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1.2 , 3.4 , 5.9 , 3.6] #output numbers treated as input for upcoming layers
-weights = [[3.9 , 7.6 , 4.3 , 1.0], # weights of each input (random number)
-          [3.6 , 7.0 , 5.0 , 1.7],
-          [6.9 , 6.6 , 4.9 , 1.8]]
+np.random.seed(0)
 
-biases = [3,2,1] #unique bias
-# weights = [3.9 , 7.6 , 4.3 , 1.0]
-# bias = 2
+X = [[1.2 , 3.4 , 5.9 , 3.6], #output numbers treated as input for upcoming layers
+          [2.0 , 5.0 , 4.1 , 2.0],
+          [-1.5 , 2.7 , 3.3 , -0.8]]
 
-output = np.dot(weights , inputs) + biases
-print(output)
+class Layer_Dense:
+    def __init__(self , n_inputs , n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs , n_neurons)
+        self.biases = np.zeros((1 , n_neurons))
+    def forward(self , inputs):
+        self.output = np.dot(inputs ,self.weights) + self.biases
+
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+
+# weights = [[3.9 , 7.6 , 4.3 , 1.0], # weights of each input (random number)
+#           [3.6 , 7.0 , 5.0 , 1.7],
+#           [6.9 , 6.6 , 4.9 , 1.8]]
+
+
+# biases = [3,2,1] #unique bias
+
+# weights2 = [[0,1 , -0.14 , 0.5], # weights of each input (random number)
+#           [-0.5 , 0.12 , -0.33],
+#           [-0.44 , 0.73 , -0.13 ]]
+
+
+# biases2 = [-1,2,-0.5] #unique bias
+# # weights = [3.9 , 7.6 , 4.3 , 1.0]
+# # bias = 2
+
+# layer1_output = np.dot(inputs , np.array(weights).T) + biases # dot product
+
+# layer2_output = np.dot(layer1_output , np.array(weights2).T) + biases2
+# print(layer2_output)
 
 # layer_outputs = []
 # for neuron_weights , neuron_bias in zip(weights , biases): # zip combines 2 list into a list of list
