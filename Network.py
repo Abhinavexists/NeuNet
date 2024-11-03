1521125ab7602e7c0038e9aa24f9a57023105b03
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-11-03 22:07:53 +0530

    Loss function added

--- Network.py
+++ Network.py
@@ -21,6 +21,26 @@ class Activation_softmax:
         probabilities = exp_values / np.sum(exp_values , axis=1 , keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output , y):
+        sample_losses = self.forward(output , y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class Loss_Categoricalcrossentropy(Loss):
+    def forward(self , y_pred , y_true):
+        sample = len(y_pred)
+        y_pred_clipped = np.clip(y_pred , 1e-7 , 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidence = y_pred_clipped[range(sample) , y_true]
+        
+        elif len(y_true.shape) == 2:
+            correct_confidence = np.sum(y_pred_clipped*y_true , axis = 1)
+
+        negative_log_likelihood = -np.log(correct_confidence)
+        return negative_log_likelihood
+        
 X,Y = create_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2,3)
@@ -36,4 +56,9 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+Loss_function = Loss_Categoricalcrossentropy()
+loss = Loss_function.calculate(activation2.output , Y)
+
+print("Loss" , loss)
 
