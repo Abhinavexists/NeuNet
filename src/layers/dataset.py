fa1c9a2ee557c75ab6638480e9ff1dd9358c64eb
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-12-09 21:57:10 +0530

    added noise - spiral dataset

--- src/layers/dataset.py
+++ src/layers/dataset.py
@@ -6,17 +6,18 @@ import matplotlib.pyplot as plt
 # spiral pattern creates a challenging, non-linear classification problem.
 np.random.seed(0)
 
-def create_data(samples=50 , classes=3):
-    X = np.zeros((samples*classes , 2))
-    Y = np.zeros(samples*classes , dtype = 'uint8')
+def create_data(samples=50, classes=3, noise=0.4):
+    X = np.zeros((samples * classes, 2))
+    Y = np.zeros(samples * classes, dtype='uint8')
     for class_number in range(classes):
-        ix = range(samples*class_number , samples*(class_number+1))
-        r = np.linspace(0.0,1 , samples) # radius
-        t = np.linspace(class_number*4 , (class_number+1)*4, samples) + np.random.randn(samples)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5) , r*np.cos(t*2.5)]
+        ix = range(samples * class_number, samples * (class_number + 1))
+        r = np.linspace(0.0, 1, samples)  # radius
+        t = np.linspace(class_number * 4, (class_number + 1) * 4, samples) + np.random.randn(samples) * noise
+        X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
         Y[ix] = class_number
-    return X ,Y
+    return X, Y
 
-print("here")
+
+print("Scatter Plot")
 
 
